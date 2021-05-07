a6d5bc3cd5fef6add63e746faa412bf2bf2240fa
anthonyluc <anthony.luc69@gmail.com>
2021-05-07 19:06:49 +0200

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
     skip_before_action :authenticate_user!, only: [:home]
 
     def home
-      @users = User.all
+      @users = User.order('random()').limit(8)
+      @users_recipes = []
+      @users.each do |u|
+        @users_recipes << Recipe.where(user: u, show: true).count
+      end
+
+      @recipes = Recipe.last(9).reverse
     end
 end
