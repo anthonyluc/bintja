1898cef2c7da617fb6330af5ce7d0a7f221239c4
anthonyluc <anthony.luc69@gmail.com>
2021-05-07 23:59:52 +0200

    followers cards

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -5,9 +5,11 @@ class FollowersController < ApplicationController
         # Follows
         follows = Follower.where(follower_id: current_user.id)
         @users = []
+        @users_recipes = []
         if follows[0] != nil
           follows.each do |f|
             @users << User.find(f.followed_id)
+            @users_recipes << Recipe.where(user: f.followed_id, show: true).count
           end
         end
         @users = Kaminari.paginate_array(@users).page(params[:p_users]).per(50)
@@ -17,9 +19,11 @@ class FollowersController < ApplicationController
         # Followers
         followers = Follower.where(followed_id: current_user.id)
         @followers = []
+        @followers_recipes = []
         if followers[0] != nil
           followers.each do |f|
             @followers << User.find(f.follower_id)
+            @followers_recipes << Recipe.where(user: f.follower_id, show: true).count
           end
         end
         @followers = Kaminari.paginate_array(@followers).page(params[:p_followers]).per(50)
