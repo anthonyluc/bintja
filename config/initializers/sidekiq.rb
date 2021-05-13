fb3119b9729b4a73b8c40e0d623a77198d6f34aa
anthonyluc <anthony.luc69@gmail.com>
2021-05-13 16:38:22 +0200

    Redis url to sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 Sidekiq.configure_server do |config|
-    config.redis = { url: 'redis://localhost:6379/0' }
+    config.redis = { url: ENV["REDISCLOUD_URL"] }
     schedule_file = "config/schedule.yml"
     if File.exists?(schedule_file)
       Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
