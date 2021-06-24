8db654d1c0209edfe76f83c435720a6f4581b9bb
anthonyluc <anthony.luc69@gmail.com>
2021-06-24 13:58:16 +0200

    return to scrap with low res thumbnails

--- app/services/scrap_videos.rb
+++ app/services/scrap_videos.rb
@@ -8,44 +8,47 @@ class ScrapVideos
         tab_title = []
 
         ### Méthode Scrapping
-        ## On transforme la requête pour YT
-        # recipe_query.gsub!(' ', '+')
-        # url = "https://www.youtube.com/results?search_query=recipe+#{recipe_query}"
-
-        # URI.open(url) {|f|
-        #     f.each_line {|line| 
-        
-        #         line.scan(/"videoRenderer":{"videoId":"([\d\D][^"]*)/) {|x|                    
-        #             tab_url << "https://www.youtube.com/watch?v=#{x.shift.strip}"
-        #         }
-                
-        #         line.scan(/"thumbnails":\[{"url":"(https:\/\/i\.ytimg\.com\/vi\/[\d\D][^?]*\.jpg)/) {|y|
-        #             tab_img << "#{y.shift.strip}"
-        #         }
-        
-        #         line.scan(/"title":{"runs":\[{"text":"([\d\D][^"]*)/) {|z|
-        #             tab_title << "#{z.shift.strip}"
-        #         }
-        #     }
-        # }
-
-        # yt_recipes_hash = {title: tab_title, url: tab_url, img: tab_img}
-
-        ### Méthode API Youtube Data v3        
         # On transforme la requête pour YT
         recipe_query.gsub!(' ', '+')
-        url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=recipe+#{recipe_query}&maxResults=48&key=#{ENV['API_YT_DATA_V3']}"
-        response_url = RestClient.get(url)
+        url = "https://www.youtube.com/results?search_query=recipe+#{recipe_query}"
 
-        response_json = JSON.parse(response_url)
-
-        response_json["items"].each do |r|
-            tab_url << "https://www.youtube.com/watch?v=#{r["id"]["videoId"]}"
-            tab_title << "#{r["snippet"]["title"]}"
-            tab_img << "#{r["snippet"]["thumbnails"]["high"]["url"]}"
-        end
+        URI.open(url) {|f|
+            f.each_line {|line| 
+        
+                line.scan(/"videoRenderer":{"videoId":"([\d\D][^"]*)/) {|x|                    
+                    tab_url << "https://www.youtube.com/watch?v=#{x.shift.strip}"
+                    
+                }
+                
+                line.scan(/"thumbnails":\[{"url":"(https:\/\/i\.ytimg\.com\/vi\/[\d\D][^?]*\.jpg)/) {|y|
+                    img_yt = y.shift.strip
+                    img_yt.gsub!('hq720', 'mqdefault')
+                    tab_img << "#{img_yt}"
+                }
+        
+                line.scan(/"title":{"runs":\[{"text":"([\d\D][^"]*)/) {|z|
+                    tab_title << "#{z.shift.strip}"
+                }
+            }
+        }
 
         yt_recipes_hash = {title: tab_title, url: tab_url, img: tab_img}
+
+        ### Méthode API Youtube Data v3        
+        # # On transforme la requête pour YT
+        # recipe_query.gsub!(' ', '+')
+        # url = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=recipe+#{recipe_query}&maxResults=48&key=#{ENV['API_YT_DATA_V3']}"
+        # response_url = RestClient.get(url)
+
+        # response_json = JSON.parse(response_url)
+
+        # response_json["items"].each do |r|
+        #     tab_url << "https://www.youtube.com/watch?v=#{r["id"]["videoId"]}"
+        #     tab_title << "#{r["snippet"]["title"]}"
+        #     tab_img << "#{r["snippet"]["thumbnails"]["high"]["url"]}"
+        # end
+
+        # yt_recipes_hash = {title: tab_title, url: tab_url, img: tab_img}
 
         return yt_recipes_hash.to_s
     end
